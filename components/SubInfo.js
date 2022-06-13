9cfeec4e22a7226d90c36f242b5ff53a0ccbdfaa
ImmanuelEgboche <77640433+ImmanuelEgboche@users.noreply.github.com>
2022-06-13 20:22:00 +0100

    adding img card cmp

--- components/SubInfo.js
+++ components/SubInfo.js
@@ -1,55 +1,68 @@
 import { View, Text } from 'react-native'
 import React from 'react'
-import { SIZES } from '../constants'
+import { SIZES, FONTS, COLORS, assets } from '../constants'
 
 export const NFTTitle = () => {
-  return (
-    <View>
-      <Text>SubInfo</Text>
-    </View>
-  )
+    return (
+        <View>
+            <Text>SubInfo</Text>
+        </View>
+    )
 }
 
 export const EthPrice = () => {
     return (
-      <View>
-        <Text>EthPrice</Text>
-      </View>
+        <View>
+            <Text>EthPrice</Text>
+        </View>
     )
 }
 
-export const ImageCmp = () => {
+export const ImageCmp = (imgUrl, index) => {
     return (
-      <View>
-        <Text>EthPrice</Text>
-      </View>
+        <Image
+        source={imgUrl}
+        resizeMode='contain'
+        style={{
+            width: 48,
+            height: 48,
+            marginLeft: index === 0 ? 0 : -SIZES.font
+        }}/>
     )
 }
 
 export const People = () => {
     return (
-      <View>
-        <Text>EthPrice</Text>
-      </View>
+        <View style={{flexDirection: 'row'}}>
+            {[assets.person02, assets.person03, assets.person04].map((imgUrl,index) => (
+                <ImageCmp/>
+            ))}
+            <Text>EthPrice</Text>
+        </View>
     )
 }
 
 export const EndDate = () => {
     return (
-      <View>
-        <Text>EthPrice</Text>
-      </View>
+        <View>
+            <Text>EthPrice</Text>
+        </View>
     )
 }
 
 export const SubInfo = () => {
     return (
-      <View style={{
-          width: '100%',
-          paddingHorizontal: SIZES.font,
-          marginTop
-      }}>
-        <Text>EthPrice</Text>
-      </View>
+        <View style={{
+            width: '100%',
+            paddingHorizontal: SIZES.font,
+            marginTop: -SIZES.extraLarge,
+            flexDirection: "row",
+            justifyContent: 'space-between'
+
+        }}>
+            <People />
+            <SubInfo />
+            <Text>EthPrice</Text>
+        </View>
     )
 }
